bba0b9116086ae33dc60a46c2e85c8fcb29b6fa8
Abraham V <abraham.varricatt@googlemail.com>
2019-09-16 19:15:33 -0400

    some echo statements

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Going to print some fancy text to make this easy to identify in netlify logs
+echo -e "\n"
+echo -e "\n"
+echo -e "\n"
+echo -e "\n"
+echo -e "\n"
+echo "------------------------------------------------------------------------"
+echo "BUILD START"
+echo "------------------------------------------------------------------------"
+
 echo "default $BUILD_TOKEN and $TOKEN_2"
 
 echo "hope those environment variables printed"
+
+
+echo "------------------------------------------------------------------------"
+echo "BUILD COMPLETED SUCCESSFULLY!"
+echo "------------------------------------------------------------------------"
