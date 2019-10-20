1598b07a61d324968a1bfc68080b8a9f933b5b9e
Alex Volkov <alex@flamy.ca>
2019-10-19 23:57:49 -0400

    Updated source paths

--- pages/talks/talk_ids.py
+++ pages/talks/talk_ids.py
@@ -4,6 +4,6 @@ List of talk IDs
 import json
 
 
-schedule_truth = "../../data/pyconca19-talks-details.json"
+schedule_truth = "../../data/speakers-details.json"
 schedule_data = json.loads((open(schedule_truth).read()))
 TALK_IDS = sorted([x["ID"] for x in schedule_data])
