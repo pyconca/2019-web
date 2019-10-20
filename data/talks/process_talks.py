acb16317a89577569050fa2c7dcc7c5ed5645258
Alex Volkov <alex@flamy.ca>
2019-10-20 10:33:52 -0400

    renamed talk_download, updated to python 3

--- data/talks/process_talks.py
+++ data/talks/process_talks.py
@@ -1,18 +1,13 @@
 # Download/Format talk information.
 # warning this is a python-2 only script.
 
-import os
 import json
 from pathlib import Path
 from jinja2 import FileSystemLoader
 from jinja2 import Environment
 from selenium import webdriver
-from talk_ids import TALK_IDS
+from .talk_ids import TALK_IDS
 
-
-URL = 'https://www.papercall.io/cfps/1311/submissions/'
-
-URL_LOGIN = 'https://www.papercall.io/signin'
 
 # Template setup
 current_folder = Path(__file__).resolve().parent
@@ -28,32 +23,20 @@ BANNED_TAGS = [
 ]
 
 # import talk data
-raw_data = open('pycon_canada_2018_submissions.json').read()
-JSON_DATA = json.loads(raw_data)
+JSON_DATA = json.loads(open('../speakers-details.json').read())
 
-try:
-    os.environ['PATH'] += ':/home/abrahamv/.local/bin'
-    user_paths = os.environ['PATH'].split(os.pathsep)
-except KeyError:
-    user_paths = []
 
 fp = webdriver.FirefoxProfile('/home/abrahamv/.mozilla/firefox/vlizshp8.default')
 
 
 # Step 1: login to papercall
-driver = webdriver.Firefox(fp)
-driver.get(URL_LOGIN)
 print('browser opened')
 
-# Step 2: login via github
-link = driver.find_element_by_link_text('GitHub')
-link.click()
-print('logging in')
 
 for talk_id in TALK_IDS:
     talk_source = talk_id.split('-')
     if talk_source[0] == 'A':
-        print('Talk = ' + talk_id + ' is not on papercall')
+        print(('Talk = ' + talk_id + ' is not on papercall'))
         continue
     # Hopefully, means it's a papercall ID
     full_url = URL + talk_source[1]
@@ -69,7 +52,7 @@ for talk_id in TALK_IDS:
             needed_details = talk_details
             break  # break only inner loop?
     if not needed_details:
-        print('Cannot find details for talk [' + talk_id + '] = ' + talk_title)
+        print(('Cannot find details for talk [' + talk_id + '] = ' + talk_title))
         continue
     # print('details found,')
     # print(needed_details)
@@ -93,10 +76,10 @@ for talk_id in TALK_IDS:
     }
     with open(Path(current_folder, filename), 'w') as text_file:
         text_file.write(template.render(context))
-    print('Auto-filled [{}]'.format(filename))
+    print(('Auto-filled [{}]'.format(filename)))
 
 
 print('script over')
 all_tags = set(FULL_TAG_LIST)
-print('tags = ' + str(all_tags))
+print(('tags = ' + str(all_tags)))
 quit()
