e15a96ad3d9a5d797e8a126d164b222fb30ba0c6
Alex Volkov <alex@flamy.ca>
2019-10-20 10:37:24 -0400

    Refactoring process talks code to load everything from speaker-details.json

--- data/talks/process_talks.py
+++ data/talks/process_talks.py
@@ -26,60 +26,55 @@ BANNED_TAGS = [
 JSON_DATA = json.loads(open('../speakers-details.json').read())
 
 
-fp = webdriver.FirefoxProfile('/home/abrahamv/.mozilla/firefox/vlizshp8.default')
+def to_yaml(talk_data):
+    pass
 
 
-# Step 1: login to papercall
-print('browser opened')
+def load_talks(json_data)
+    pass
 
 
-for talk_id in TALK_IDS:
-    talk_source = talk_id.split('-')
-    if talk_source[0] == 'A':
-        print(('Talk = ' + talk_id + ' is not on papercall'))
-        continue
-    # Hopefully, means it's a papercall ID
-    full_url = URL + talk_source[1]
-    # Step 3: Open paper details link
-    driver.get(full_url)
-    # Step 4: Grab paper title
-    temp = driver.find_element_by_xpath("/html/body/div/div[1]/div/div/div[1]/h1")
-    talk_title = temp.text
-    # Step 5: Scan JSON for talk details
-    needed_details = None
-    for talk_details in JSON_DATA:
-        if talk_details['title'] == talk_title:
-            needed_details = talk_details
-            break  # break only inner loop?
-    if not needed_details:
-        print(('Cannot find details for talk [' + talk_id + '] = ' + talk_title))
-        continue
-    # print('details found,')
-    # print(needed_details)
+def old_code():
+    for talk_id in TALK_IDS:
+        # Hopefully, means it's a papercall ID
+        needed_details = None
+        for talk_details in JSON_DATA:
+            if talk_details['title'] == talk_title:
+                needed_details = talk_details
+                break  # break only inner loop?
+        if not needed_details:
+            print(('Cannot find details for talk [' + talk_id + '] = ' + talk_title))
+            continue
+        # print('details found,')
+        # print(needed_details)
 
-    # Step 6: Fill data in YAML file
-    filename = str(talk_id) + '.yaml'
-    talk_tags = needed_details.get('tags', [])
-    for tag in BANNED_TAGS:
-        try:
-            talk_tags.remove(tag)
-        except ValueError:
-            pass  # if not found, don't worry
-    FULL_TAG_LIST.extend(talk_tags)
+        # Step 6: Fill data in YAML file
+        filename = str(talk_id) + '.yaml'
+        talk_tags = needed_details.get('tags', [])
+        for tag in BANNED_TAGS:
+            try:
+                talk_tags.remove(tag)
+            except ValueError:
+                pass  # if not found, don't worry
+        FULL_TAG_LIST.extend(talk_tags)
 
-    context = {
-        'name': needed_details.get('name', '').replace('"', "'"),
-        'title': needed_details.get('title', '').replace('"', "'"),
-        'abstract': needed_details.get('abstract', '').replace('"', "'"),
-        'details': needed_details.get('description', '').replace('"', "'"),
-        'talk_tags': talk_tags,
-    }
-    with open(Path(current_folder, filename), 'w') as text_file:
-        text_file.write(template.render(context))
-    print(('Auto-filled [{}]'.format(filename)))
+        context = {
+            'name': needed_details.get('name', '').replace('"', "'"),
+            'title': needed_details.get('title', '').replace('"', "'"),
+            'abstract': needed_details.get('abstract', '').replace('"', "'"),
+            'details': needed_details.get('description', '').replace('"', "'"),
+            'talk_tags': talk_tags,
+        }
+        with open(Path(current_folder, filename), 'w') as text_file:
+            text_file.write(template.render(context))
+        print(('Auto-filled [{}]'.format(filename)))
 
 
-print('script over')
-all_tags = set(FULL_TAG_LIST)
-print(('tags = ' + str(all_tags)))
-quit()
+    print('script over')
+    all_tags = set(FULL_TAG_LIST)
+    print(('tags = ' + str(all_tags)))
+    quit()
+
+
+if __name__ == "__main__":
+    pass
